2209d08057fd882b39118f9a5f8e00f839e05f9a
KEO <keoxcoelho@gmail.com>
2025-07-02 20:43:51 -0300

    fix: Inserido valor não nulo em Suite

--- ProjetoReservasNoHotel/HotelReserva/Models/Suite.cs
+++ ProjetoReservasNoHotel/HotelReserva/Models/Suite.cs
@@ -2,9 +2,9 @@ namespace HotelReserva.Models;
 
 public class Suite
 {
-    public string TipoSuite { get; set; }
-    public int Capacidade { get; set; }
-    public decimal ValorDiaria { get; set; }
+    public string TipoSuite { get; set; } = string.Empty;
+    public int Capacidade { get; set; } = 0; 
+    public decimal ValorDiaria { get; set; } = 0;
 
     public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
     {
