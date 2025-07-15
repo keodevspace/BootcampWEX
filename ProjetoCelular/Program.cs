6eb00a5fe9147f2f7492425ff50b7990f7179da7
KEO <keoxcoelho@gmail.com>
2025-07-14 23:26:51 -0300

    feat: Implement inheritance and override method in Iphone and Nokia classes

--- ProjetoCelular/Program.cs
+++ ProjetoCelular/Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia("111111111", "Nokia C20", "00000000001", 64);
+        Smartphone iphone = new Iphone("222222222", "iPhone 13", "00000000002", 128);
+
+        Console.WriteLine("Testando Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Testando iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
